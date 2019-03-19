4f47cf50e802b436ddb00cad6280b2143d249fdf
James Rudrum <james_rudrum@outlook.com>
2019-03-19 15:05:37 +0000

    :construction: Add entering personal info feature test

--- app/src/androidTest/java/com/example/hoot/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/hoot/ExampleInstrumentedTest.java
@@ -1,12 +1,16 @@
 package com.example.hoot;
-
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +20,25 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule;
+
+    public ExampleInstrumentedTest() {
+        activityRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+    }
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.hoot", appContext.getPackageName());
+    }
+
+    @Test
+    public void userCanEnterFirstName() {
+        onView(withId(R.id.firstName)).perform(typeText("Erin"));
+        onView(withId(R.id.email)).perform(typeText("erin@google.com"));
     }
 }
