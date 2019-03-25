a0ed1ea182f8ecda192db0ffcf2ffdc8c44a1e6b
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-03-25 13:39:35 +0000

    :sparkles: shows the opposite account types in feed
    Co-authored-by: Brooke Woolley <brooke.woolley@gmail.com>

--- app/src/main/java/com/example/hoot/FeedActivity.java
+++ app/src/main/java/com/example/hoot/FeedActivity.java
@@ -1,5 +1,6 @@
 package com.example.hoot;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,14 +9,20 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class FeedActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     FirebaseDatabase database;
     DatabaseReference myRef;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,30 +35,59 @@ public class FeedActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
-
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
         database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("users/wise");
+
+        database.getReference("users").addListenerForSingleValueEvent(new ValueEventListener() {
+            FirebaseUser user = mAuth.getCurrentUser();
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child("young").child(user.getUid()).exists()) {
+                    myRef = database.getReference("users/wise");
+                    FirebaseRecyclerAdapter<RecyclerModelActivity, ViewHolderActivity> firebaseRecyclerAdapter =
+                            new FirebaseRecyclerAdapter<RecyclerModelActivity, ViewHolderActivity>(
+                                    RecyclerModelActivity.class,
+                                    R.layout.feed_card,
+                                    ViewHolderActivity.class,
+                                    myRef
+                            ) {
+                                @Override
+                                protected void populateViewHolder(ViewHolderActivity ViewHolderActivity, RecyclerModelActivity RecyclerModelActivity, int position) {
+                                    ViewHolderActivity.setDetails(getApplicationContext(), RecyclerModelActivity.getName(), RecyclerModelActivity.getAboutme(), RecyclerModelActivity.getImage());
+                                }
+                            };
+                    recyclerView.setAdapter(firebaseRecyclerAdapter);
+                } else if (dataSnapshot.child("wise").child(user.getUid()).exists()) {
+                    myRef = database.getReference("users/young");
+                    FirebaseRecyclerAdapter<RecyclerModelActivity, ViewHolderActivity> firebaseRecyclerAdapter =
+                            new FirebaseRecyclerAdapter<RecyclerModelActivity, ViewHolderActivity>(
+                                    RecyclerModelActivity.class,
+                                    R.layout.feed_card,
+                                    ViewHolderActivity.class,
+                                    myRef
+                            ) {
+                                @Override
+                                protected void populateViewHolder(ViewHolderActivity ViewHolderActivity, RecyclerModelActivity RecyclerModelActivity, int position) {
+                                    ViewHolderActivity.setDetails(getApplicationContext(), RecyclerModelActivity.getName(), RecyclerModelActivity.getAboutme(), RecyclerModelActivity.getImage());
+                                }
+                            };
+                    recyclerView.setAdapter(firebaseRecyclerAdapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        FirebaseRecyclerAdapter<RecyclerModelActivity, ViewHolderActivity> firebaseRecyclerAdapter =
-                new FirebaseRecyclerAdapter<RecyclerModelActivity, ViewHolderActivity>(
-                        RecyclerModelActivity.class,
-                        R.layout.feed_card,
-                        ViewHolderActivity.class,
-                        myRef
-                ) {
-                    @Override
-                    protected void populateViewHolder(ViewHolderActivity ViewHolderActivity, RecyclerModelActivity RecyclerModelActivity, int position) {
-                         ViewHolderActivity.setDetails(getApplicationContext(), RecyclerModelActivity.getName(), RecyclerModelActivity.getAboutme(), RecyclerModelActivity.getImage());
-                    }
-                };
-        recyclerView.setAdapter(firebaseRecyclerAdapter);
+
     }
-
-
 }
