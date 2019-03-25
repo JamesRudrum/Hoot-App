02165d3a9eaea8030a7c446ff1d4b90f88f82675
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-03-25 21:10:36 +0000

    :sparkles: view profile button leads to request contact page

--- app/src/main/java/com/example/hoot/FeedActivity.java
+++ app/src/main/java/com/example/hoot/FeedActivity.java
@@ -1,5 +1,6 @@
 package com.example.hoot;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,8 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
@@ -30,7 +33,6 @@ public class FeedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_feed);
 
         ActionBar actionBar = getSupportActionBar();
-
 //        actionBar.setTitle("Feed");
 
         recyclerView = findViewById(R.id.recyclerView);
@@ -43,8 +45,9 @@ public class FeedActivity extends AppCompatActivity {
         database.getReference("users").addListenerForSingleValueEvent(new ValueEventListener() {
             FirebaseUser user = mAuth.getCurrentUser();
 
+
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            public void onDataChange(@NonNull final DataSnapshot dataSnapshot) {
                 if (dataSnapshot.child("young").child(user.getUid()).exists()) {
                     myRef = database.getReference("users/wise");
                     FirebaseRecyclerAdapter<RecyclerModelActivity, ViewHolderActivity> firebaseRecyclerAdapter =
@@ -55,8 +58,9 @@ public class FeedActivity extends AppCompatActivity {
                                     myRef
                             ) {
                                 @Override
+
                                 protected void populateViewHolder(ViewHolderActivity ViewHolderActivity, RecyclerModelActivity RecyclerModelActivity, int position) {
-                                    ViewHolderActivity.setDetails(getApplicationContext(), RecyclerModelActivity.getName(), RecyclerModelActivity.getAboutme(), RecyclerModelActivity.getImage());
+                                    ViewHolderActivity.setDetails(FeedActivity.this, RecyclerModelActivity.getName(), RecyclerModelActivity.getAboutme(), RecyclerModelActivity.getImage());
                                 }
                             };
                     recyclerView.setAdapter(firebaseRecyclerAdapter);
@@ -71,7 +75,7 @@ public class FeedActivity extends AppCompatActivity {
                             ) {
                                 @Override
                                 protected void populateViewHolder(ViewHolderActivity ViewHolderActivity, RecyclerModelActivity RecyclerModelActivity, int position) {
-                                    ViewHolderActivity.setDetails(getApplicationContext(), RecyclerModelActivity.getName(), RecyclerModelActivity.getAboutme(), RecyclerModelActivity.getImage());
+                                    ViewHolderActivity.setDetails(FeedActivity.this, RecyclerModelActivity.getName(), RecyclerModelActivity.getAboutme(), RecyclerModelActivity.getImage());
                                 }
                             };
                     recyclerView.setAdapter(firebaseRecyclerAdapter);
@@ -88,6 +92,5 @@ public class FeedActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-
     }
 }
--- app/src/main/java/com/example/hoot/ViewHolderActivity.java
+++ app/src/main/java/com/example/hoot/ViewHolderActivity.java
@@ -1,9 +1,11 @@
 package com.example.hoot;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,14 +17,21 @@ public class ViewHolderActivity extends RecyclerView.ViewHolder {
 
     public ViewHolderActivity(View itemView) {
         super(itemView);
-
         view = itemView;
     }
 
-    public void setDetails(Context ctx, String name, String aboutme, String image){
+    public void setDetails(final Context ctx, String name, String aboutme, String image){
         TextView nameView = view.findViewById(R.id.feedName);
         TextView aboutMeView = view.findViewById(R.id.feedAboutMe);
         ImageView imageView = view.findViewById(R.id.feedProfileImage);
+        Button BTNViewProfile = view.findViewById(R.id.BTNViewProfile);
+
+        BTNViewProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ctx.startActivity(new Intent(ctx, RequestContactPageActivity.class));
+            }
+        });
 
         nameView.setText(name);
         aboutMeView.setText(aboutme);
