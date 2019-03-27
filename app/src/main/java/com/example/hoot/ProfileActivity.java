c49d2e259853412bb3472c0aa116ce67cdf9421c
Brooke Woolley <brooke.woolley@gmail.com>
2019-03-27 18:19:12 +0000

    :construction: implementing menu toggle

--- app/src/main/java/com/example/hoot/ProfileActivity.java
+++ app/src/main/java/com/example/hoot/ProfileActivity.java
@@ -3,8 +3,12 @@ package com.example.hoot;
 import android.os.Bundle;
 
 import android.support.annotation.NonNull;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -35,6 +39,9 @@ public class ProfileActivity extends AppCompatActivity {
     private TextView TVinterestsListProfilePage;
     private List<String> interestList;
     private TextView TVMyInterests;
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
+
 
 
     @Override
@@ -54,8 +61,13 @@ public class ProfileActivity extends AppCompatActivity {
         TVAboutMeProfile = findViewById(R.id.TVAboutMeProfile);
         TVinterestsListProfilePage = findViewById(R.id.TVinterestsListProfilePage);
         TVMyInterests = findViewById(R.id.TVMyInterests);
+        drawer = findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this,drawer, R.string.navigation_drawer_open,      R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         GlideApp.with(ProfileActivity.this).load(profileImageStorageReference).into(IVprofilePagePicture);
+
 
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             FirebaseUser user = mAuth.getCurrentUser();
@@ -129,5 +141,13 @@ public class ProfileActivity extends AppCompatActivity {
 
     }
 
-   }
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
 
