edc7b0f03a13908d3710c2845cb99cfc2d5c9d61
James Rudrum <james_rudrum@outlook.com>
2019-03-29 13:48:10 +0000

    :lipstick: Add splash screen

---
+++ app/src/main/java/com/example/hoot/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.example.hoot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread loading = new Thread() {
+            public void run() {
+                try {
+                    sleep(2000);
+                    Intent main = new   Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(main);
+                    finish();
+
+
+                }
+
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                finally {
+                    finish();
+                }
+            }
+        };
+
+        loading.start();
+    }
+
+};
